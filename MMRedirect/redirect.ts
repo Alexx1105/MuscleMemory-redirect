0b83afc464639eadb8bf8d237c66b8568b9ab9ff
Alexx1105 <alexh2877@gmail.com>
2024-11-16 23:40:00 -0500

    deployed redirect function

--- MMRedirect/redirect.ts
+++ MMRedirect/redirect.ts
@@ -1,3 +1,14 @@
-  let redirect = () => {
-    return("manroop the jatt")
+ 
+ 
+ 
+ let redirect = (url, asLink = false) => {          //false is for an http redirect 
+  asLink ? (window.location.href) : window.location.replace(url);
+  redirect("MuscleMemory.KimchiLabs.com://");
+
+  if (window.location.replace !== url) {
+    console.error("redirect link not not work");
+    return("error");
   };
+ };
+
+
