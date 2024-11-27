1a71ed4186903f82051f80c1dfb9963d342a5f7f
Alexx1105 <alexh2877@gmail.com>
2024-11-26 19:39:50 -0500

    fixed

--- api/redirect.ts
+++ api/redirect.ts
@@ -1,19 +1,23 @@
 
 
 
-const redirect = (url, asLink = false) => {          //false is for an http redirect 
+const redirect = (url: string, asLink = false): Promise<Response> => {               //false is for an http redirect 
   try {
       if (asLink) {
-        window.location.href = url;
+        return Promise.resolve(new Response(null, { status: 301, headers: { Location: url }, }));    //server side redirect
       } else {
         window.location.replace(url);
+        return Promise.resolve(new Response("Client-side redirect initiated", { status: 200 }));      //initiate redirect
       }
-      } catch (error) {
-          console.error("error 404", error);
+       } catch (error) {
+          return Promise.resolve(new Response("error 500, redirect did not go through:", {status: 500}));    //failure case for redirect
       }   
 };
 
-const main = () => {
-redirect("MuscleMemory.KimchiLabs.com://", false);
+const main = async() => {
+const redir = await redirect("MuscleMemory.KimchiLabs.com://", false);
+if (redir.status === 301) {
+  console.log("successful redirect", {redir})
+  }
 };
 main();
